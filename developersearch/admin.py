eabbd70dfbab4fedcdb995ff2f42e0eccfb1462c
wasim2263 <wasim@squaredworks.com>
2018-09-30 13:23:18 +0600

    database design complete

--- developersearch/admin.py
+++ developersearch/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Developers, Interviews, Languages, ProgrammingLanguages
+
+admin.site.register(Developers)
+admin.site.register(Interviews)
+admin.site.register(Languages)
+admin.site.register(ProgrammingLanguages)
--- developersearch/models.py
+++ developersearch/models.py
@@ -6,19 +6,32 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Developers(models.Model):
     email = models.EmailField(max_length=254, unique=True, null=False)
 
+    class Meta:
+        db_table = 'developers'
+
+
 
 class ProgrammingLanguages(models.Model):
     developer = models.ManyToManyField(Developers)
     name = models.CharField(max_length=254, unique=True, null=False)
 
+    class Meta:
+        db_table = 'programming_languages'
+
 class Languages(models.Model):
     developer = models.ManyToManyField(Developers)
     code = models.CharField(max_length=254, unique=True, null=False)
+
+    class Meta:
+        db_table = 'languages'
 
 class Interviews(models.Model):
     developer = models.ForeignKey(Developers, on_delete=models.CASCADE)
     score = models.PositiveIntegerField(validators=[MinValueValidator(1), MinValueValidator(5)])
     comment = models.TextField(max_length=500)
+
+    class Meta:
+        db_table = 'interviews'
 
 
 
